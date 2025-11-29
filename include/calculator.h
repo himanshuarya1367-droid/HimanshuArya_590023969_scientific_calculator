a819d814fc0ea5fb26cd22de81b6e640e433a031
himanshuarya1367-droid <himanshuarya1367@gmail.com>
2025-11-29 17:48:37 +0530

    Add files via upload

---
+++ include/calculator.h
@@ -0,0 +1,19 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+double power(double base, double exp);
+double squareroot(double num);
+
+double sine_val(double x);
+double cosine_val(double x);
+double tangent_val(double x);
+
+double natural_log(double x);
+double log_base10(double x);
+
+#endif
